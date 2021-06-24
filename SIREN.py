9c5570753ee9d05382e79f5560de70943f3d6d20
JHLew <fudojhl@gmail.com>
2021-06-24 22:22:32 +0900

    initial commit

---
+++ SIREN.py
@@ -0,0 +1,102 @@
+import torch
+from torch import nn
+from einops import rearrange
+import numpy as np
+
+
+# normalize (0, 1) to (-1, 1)
+def preprocess(t):
+    return t * 2 - 1
+
+# denormalize from (-1, 1) to (0, 1)
+def postprocess(t):
+    return torch.clamp((t + 1) / 2, min=0, max=1)
+
+
+class InputMapping:
+    def __init__(self, mapping_size=256, B='gaussian', sigma=10):
+        if B == 'gaussian':
+            self.B = torch.randn((mapping_size, 2), requires_grad=True) * sigma
+        else:
+            self.B = None
+
+    def map(self, x):
+        if self.B is None:
+            return x
+        else:
+            x_proj = torch.bmm((2 * np.pi * x), self.B.T)
+            return torch.cat([torch.cos(x_proj), torch.sin(x_proj)], dim=-1)
+
+
+def uniform_coordinates(w, h):
+    coords = [torch.linspace(-1, 1, steps=h), torch.linspace(-1, 1, steps=w)]
+    mgrid = torch.stack(torch.meshgrid(*coords), dim=-1)
+    mgrid = rearrange(mgrid, 'h w c -> (h w) c')
+
+    return mgrid.detach()
+
+
+class Siren(nn.Module):
+    def __init__(self, dim_in, dim_out, w0=1., c=6., is_first=False):
+        super().__init__()
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        self.is_first = is_first
+
+        self.init_wb(c=c, w0=w0)
+        self.w0 = w0
+        self.c = c
+        self.is_first = is_first
+
+    def init_wb(self, c, w0):
+        self.fc = nn.Linear(self.dim_in, self.dim_out)
+
+        w_std = 1 / self.dim_in if self.is_first else np.sqrt(c / self.dim_in) / w0
+
+        self.fc.weight.data.uniform_(-w_std, w_std)
+        self.fc.bias.data.uniform_(-w_std, w_std)
+
+    def forward(self, x):
+        out = self.fc(x)
+        if self.is_first:
+            out = self.w0 * out
+        out = torch.sin(out)
+        return out
+
+
+class SirenNet(nn.Module):
+    def __init__(self, dim_in, dim_hidden, dim_out, n_hidden_layers, w0=1., w0_initial=30., c=6.):
+        super().__init__()
+        self.num_layers = n_hidden_layers
+        self.dim_hidden = dim_hidden
+
+        layers = []
+        is_first = True
+        for _ in range(self.num_layers):
+            layer_w0 = w0_initial if is_first else w0
+            layer_dim_in = dim_in if is_first else dim_hidden
+
+            layers.append(Siren(
+                dim_in=layer_dim_in,
+                dim_out=dim_hidden,
+                w0=layer_w0,
+                c=c,
+                is_first=is_first,
+            ))
+
+            if is_first:
+                is_first = False
+
+        final_layer = nn.Linear(dim_hidden, dim_out)
+        w_std = np.sqrt(c / dim_hidden) / w0
+        final_layer.weight.data.uniform_(-w_std, w_std)
+        layers.append(final_layer)
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x=None, out_size=None):
+        out = self.layers(x)
+        w, h = out_size
+        out = rearrange(out, '(h w) c -> () c h w', h=h, w=w)
+
+        return postprocess(out)
+
---
+++ train.py
@@ -0,0 +1,78 @@
+from PIL import Image
+from torchvision.transforms.functional import to_tensor, to_pil_image
+from torch.nn.functional import mse_loss
+import torch.optim as optim
+import os
+import shutil
+
+from SIREN import *
+
+
+def train(net, target_img, n_iters, save_path, validation_path=None):
+    # preparation: clean validation directory
+    if validation_path is None:
+        validation_path = './validation'
+    else:
+        if os.path.exists(validation_path):
+            shutil.rmtree(validation_path)
+        os.makedirs(validation_path)
+
+    optimizer = optim.Adam(net.parameters(), lr=1e-3)
+
+    img = to_tensor(target_img).unsqueeze(0).cuda()
+    h, w = img.shape[2:]
+
+    print('start training...')
+    for i in range(n_iters):
+        coords = uniform_coordinates(w, h).cuda()
+        output = net(coords, (w, h))
+        loss = mse_loss(output, img)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 2500 == 0:
+            print(i + 1, loss.item())
+
+            with torch.no_grad():
+                coords = uniform_coordinates(w, h).cuda()
+                output = net(coords, (w, h))
+
+            to_pil_image(output[0].cpu()).save('{}/{}.png'.format(validation_path, i + 1))
+            torch.save(net.state_dict(), save_path)
+
+    coords = uniform_coordinates(w, h).cuda()
+    output = net(coords, (w, h))
+    to_pil_image(output[0].cpu()).save('{}/final.png'.format(validation_path))
+
+    torch.save(net.state_dict(), save_path)
+
+
+def inference(net, size):
+    w, h = size
+    with torch.no_grad():
+        coords = uniform_coordinates(w, h).cuda()
+        output = net(coords, (w, h))
+
+    to_pil_image(output[0].cpu()).save('./for_fun.png')
+
+
+if __name__ == '__main__':
+    is_train = True
+    net = SirenNet(dim_in=2, dim_hidden=256, dim_out=3, n_hidden_layers=5)
+    img = Image.open('./examples/0002x4.png')
+    w, h = img.size
+    n_iters = 25000
+    save_path = './for_fun.pth'
+    validation_path = './validation_for_fun'
+
+    if is_train:
+        train(net.cuda(), img, n_iters, save_path, validation_path)
+
+    else:
+        net.load_state_dict(torch.load(save_path))
+        inference(net.cuda(), size=(w, h))
+
+
+
